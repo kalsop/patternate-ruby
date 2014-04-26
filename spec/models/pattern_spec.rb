12f33355ac4a08d3ec515bfd2ecc7c28a5bc6479
kalsop <katherine.alsop@gmail.com>
2014-04-26 16:06:43 +0100

    ag/ka - fixing model spec

--- spec/models/pattern_spec.rb
+++ spec/models/pattern_spec.rb
@@ -6,7 +6,7 @@ describe Pattern do
     pattern_company = PatternCompany.create(name: "Vogue")
     pattern_collection = PatternCollection.create(pattern_company: pattern_company, name: "Anne Klein")
     pattern = Pattern.create(pattern_company: pattern_company, pattern_collection: pattern_collection, pattern_number: '1234')
-    expect(pattern.display_name).to eq "Vogue (Anne Klein) 1234"
+    expect(pattern.display_name).to eq "Vogue 1234 - Anne Klein"
   end
   
   it "should display the company and number when pattern is not part of a collection" do
