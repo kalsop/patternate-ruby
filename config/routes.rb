0ae6527be2c9f9e064b845ea34f16e8fa552cdb5
kalsop <katherine.alsop@gmail.com>
2014-04-26 13:38:10 +0100

    Revert "ka/ag made the app display at the root"
    This reverts commit fc6a28f48df08f505398c62397c1d04f290208f3.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 PatternateRuby::Application.routes.draw do
   
-  root 'patterns#index'
   resources :patterns
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
