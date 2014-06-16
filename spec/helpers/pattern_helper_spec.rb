a8f803e42f1824477aa0e32f46c941499ce9bc08
kalsop <katherine.alsop@gmail.com>
2014-06-16 21:45:10 +0100

    Updated PatternViewHelper test to reflect refactoring

--- spec/helpers/pattern_helper_spec.rb
+++ spec/helpers/pattern_helper_spec.rb
@@ -1,21 +1,21 @@
 require 'spec_helper'
 
-describe PatternHelper do
+describe PatternViewHelper do
 
   it "should display correct message if there are search terms but no results" do
     @has_results = false
-    @search_terms = ["dress"]
+    @existing_search_terms = ["dress"]
     expect(helper.result_summary).to include PatternConstants.const_get(:NO_RESULTS_MESSAGE)
   end
   
   it "should display correct message if there are search terms and results" do
     @has_results = true
-    @search_terms = ["dress"]
+    @existing_search_terms = ["dress"]
     expect(helper.result_summary).to include PatternConstants.const_get(:RESULTS_MESSAGE)
   end
   
   it "should display correct message if there are no search terms" do
-    @search_terms = []
+    @existing_search_terms = []
     expect(helper.result_summary).to include PatternConstants.const_get(:NO_SEARCH_TERMS_MESSAGE)
   end
 
