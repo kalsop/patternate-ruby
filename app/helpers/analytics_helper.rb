80f727bb53a779d4ee2825563e860b18e4f9acb2
kalsop <katherine.alsop@gmail.com>
2014-06-19 22:16:32 +0100

    [ag/ka] fixed first visit cookie bug

--- app/helpers/analytics_helper.rb
+++ app/helpers/analytics_helper.rb
@@ -2,7 +2,7 @@ module AnalyticsHelper
   
   def assign_date_of_first_visit first_visit_cookie
    if first_visit_cookie.nil?
-     Date.today.to_s
+     return Date.today.to_s
    end 
    first_visit_cookie   
   end
