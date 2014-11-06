03905b8fb50fa8da7a0999f9aabb73f9a7e06c3b
kalsop <katherine.alsop@gmail.com>
2014-11-06 13:49:09 +0000

    [ka] Search now works based on tags and autosuggest for a single term at a time

--- app/models/pattern.rb
+++ app/models/pattern.rb
@@ -13,7 +13,11 @@ patterns_matching_at_least_one_style = []
 
 terms.each do |style|
   
-  find_patterns_matching_at_least_one_style_query = "SELECT patterns.* FROM patterns INNER JOIN patterns_styles ON (patterns_styles.pattern_id = patterns.id) WHERE (style_id = '#{style}')"
+  find_patterns_matching_at_least_one_style_query = "
+    SELECT p.* FROM patterns p
+    INNER JOIN patterns_styles ps ON (ps.pattern_id = p.id) 
+    INNER JOIN styles s ON (ps.style_id = s.id)
+    WHERE (s.name = '#{style}')"
   find_patterns_matching_at_least_one_style = ActiveRecord::Base.connection.execute(find_patterns_matching_at_least_one_style_query)
   find_patterns_matching_at_least_one_style.each do |row|
     patterns_matching_at_least_one_style << "#{row['id']}"
@@ -30,68 +34,11 @@ hash_of_patterns_matching_all_styles.each do |match|
 end
 
 self.find(patterns_matching_all_styles)
-# all_the_patterns = []
-# patterns_matching_all_styles.each do | pattern | 
-#   single_pattern = self.where(patterns: {id: pattern})
-#   # this creates an array
-#   all_the_patterns << single_pattern
-#   # then this is an array of arrays
-# end
-# 
-# all_the_patterns.each do |kkk|
-#   puts kkk
-# end
+
 
 
 }
 
-# File.open('/Users/kalsop/Documents/Personal/Dev/patternate/patternate-ruby/scraper/results', 'a+') do | file |
-#   patterns_matching_all_styles.each do | value | 
-#     file.write("#{value} || ")
-#   end
-#   file.write "\n\n" 
-# end
-
-# 
-#     number_of_terms = terms.count
-#     composed_scope = self.scoped.joins(:style)
-# 
-#     terms.each do |term|
-# 
-# 
-#       composed_scope = composed_scope.where(styles: {id: term})
-# # this only work with one query. it tries to do 'where style_id = 3 and style_id = 2'
-#     
-# 
-#     end
-# 
-#     composed_scope
-
-
-  
-
-
-
-# old functioning search
-# 
-# 
-  # scope :fuzzy_search, lambda { |terms|
-  #   return self.order('id DESC') if terms.empty?
-  #  
-  #   composed_scope = self.scoped.joins(:pattern_company).order('id DESC')
-  #      
-  #   terms.each do |term|
-  #     term_in_description = '% ' << term << '%'
-  #     term = '%' << term << '%'
-  #     composed_scope = composed_scope.where(
-  #     'description ILIKE :term_in_description 
-  #     OR pattern_number ILIKE :term 
-  #     OR pattern_name ILIKE :term 
-  #     OR pattern_company.name ILIKE :term', {:term => term, :term_in_description => term_in_description})
-  #   end
-  #  
-  #   composed_scope
-  # }
 
 
   def display_name
